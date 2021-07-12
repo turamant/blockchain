63049de49c95c8480edecbf7bc61b3223e3ac0e7
turamant <tur1amant@gmail.com>
2021-07-12 19:58:02 +0300

    main.py

--- src/my_flask/main.py
+++ src/my_flask/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, url_for
 from flask import request
+
+
 from src.my_flask.block import *
 
 app = Flask(__name__)
@@ -14,8 +16,17 @@ def index():
         borrower = request.form['borrower']
 
         write_block(name=lender, amount=amount, to_whom=borrower)
+        return redirect(url_for("index"))
 
     return render_template("index.html")
+
+
+@app.route('/checking', methods=['GET'])
+def check():
+    results = check_integrity()
+    return render_template('index.html', results=results)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
